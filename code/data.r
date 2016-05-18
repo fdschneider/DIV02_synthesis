03efe357b33e8885abd67cf6d5afd07a27e9baaf
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-05-18 12:33:00 +0200

    init data.r : load data and document sources

---
+++ code/data.r
@@ -0,0 +1,8 @@
+# read in data
+
+plants_full <- read.csv2("data/plants_core/plantcover.csv") 
+# original file: 19686.xlsx  downloaded from   BExis on 17.05.2016 as Bexis dataset 19686 vegetation relevés EP 2008-2015 1.2.5 ; Maintainer: Markus Fischer
+
+arthropod_traits <- read.csv2("data/arthropod_traits/ArthropodSpeciesTraits_format.csv")
+# Original file: ArthropodSpeciesTraits.csv   from   Gossner MM, Simons NK, Achtziger R, Blick T, Dorow WHO, Dziock F, Köhler F, Rabitsch W, Weisser WW (2015) Data from: A summary of eight traits of Coleoptera, Hemiptera, Orthoptera and Araneae, occurring in grasslands in Germany. Dryad Digital Repository. http://dx.doi.org/10.5061/dryad.53ds2
+
