503e6c7bab5817554cca7b6c46742ebd9b57a15d
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-09-14 16:41:15 +0200

    calculate plant and arthropod weighted means

--- code/weightedmeans.r
+++ code/weightedmeans.r
@@ -1,24 +1,64 @@
 ### get community weighted means
-library(plyr)
 
+source("R/cwm.r")
 source("code/data.r")
-
-cwm <- function(abundances, traits = c("Body_Size", "Dispersal_ability", "Stratum_use_numeric"), trait_table = arthropod_traits) {
-  temp <- cbind(abundances[,c("Species", "mean")], trait_table[match(abundances$Species, trait_table$SpeciesID),traits])
-  
-  output <- colSums(temp[,traits]*temp$mean, na.rm = TRUE)/sum(temp$mean, na.rm = TRUE)
-  
-}
-
-arhropods_annual <- ddply(arthropods_core[,c("PlotID", "Species","CollectionYear","NumberAdults")], .(PlotID, Species, CollectionYear), summarize, mean = round(mean(NumberAdults, na.rm = TRUE),2) )
-
-cwm_arthropods <- ddply(arhropods_annual, .(PlotID,CollectionYear), cwm)
-
-pairs(cwm_arthropods[3:5])
+library(foreach)
+library(doSNOW)
+################## calculate plant CWM values per plot per year. 
 
 
+#workerlist <- c(rep("localhost", times = 3))
+#cl <- makeSOCKcluster(workerlist)
+#registerDoSNOW(cl)
 
-plants_annual <- ddply(plants_full[,c("Plotid", "Species","Year","cover")], .(Plotid, Species, Year), summarize, mean = round(mean(cover, na.rm = TRUE),2) )
+#plants_annual <- ddply(plants_full[,c("EP_PlotId", "Species","Year","cover")], .(EP_PlotId, Species, Year), summarize, mean = round(mean(cover, na.rm = TRUE),2), .drop = TRUE )
 
-cwm_plants <- ddply(plants_annual, .(Plotid,Year), cwm, trait_table = plant_traits, traits = c("SLA", "leafPpermass", "leafNpermass", LDMC) )
+#stopCluster(cl)
 
+cwm_plants <- ddply(plants_full, .(EP_PlotId,Year), cwm, trait_table = plant_trait_matrix, traits = c("SLA", "leaf_P", "leaf_N", "LDMC", "leaf_area", "seedmass","SSD"), abund_label = "cover", spec_label = "SpeciesID", trait_spec_label= "AccSpeciesID")
+
+save(cwm_plants, file = "data/cwm_plants.rData")
+
+# 
+# {
+# p <- "HEG12"
+# y <- "2008"
+# s <- "Orius niger" "
+# x <- subset(arthropods_core, PlotID == p & CollectionYear == y & Species == s & selector == TRUE, .drop = TRUE)
+# 
+# 
+# out <- data.frame(
+#   Species = arthropod_species,
+#   NumberAdults = rep(0, length(arthropod_species))
+# )
+# 
+# out$NumberAdults[match(x$Species ,  arthropod_species)] <- x$NumberAdults
+# arthropod_species[!arthropod_species %in% heg12$Species]
+# }
+
+
+## WARNING: Check calculation of annual means: species that have not been reported should have value 0 at each plot and not be treated as NA.  
+arthropods_annual <- ddply(arthropods_core[arthropods_core$selector == TRUE,c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, Species, CollectionYear), summarize, mean = round(sum(NumberAdults, na.rm = TRUE)/2 ,4), .drop = TRUE )
+
+cwm_arthropods <- ddply(arhropods_annual, .(PlotID,CollectionYear), cwm, trait_table = arthropod_traits, traits = c("Body_Size", "Dispersal_ability", "Stratum_use_numeric"))
+
+pairs(cwm_arthropods[3:5], pch = 20)
+
+
+
+herbivores_annual <- ddply(herbivores[herbivores$selector == TRUE,c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, Species, CollectionYear), summarize, mean = round(sum(NumberAdults, na.rm = TRUE)/2 ,4), .drop = TRUE )
+
+cwm_herbivores <- ddply(herbivores_annual, .(PlotID,CollectionYear), cwm, trait_table = arthropod_traits, traits = c("Body_Size", "Dispersal_ability", "Stratum_use_numeric", "Feeding_mode_numeric"))
+
+pairs(cwm_herbivores[3:6], pch = 20)
+
+
+
+
+consumers_annual <- ddply(consumers[consumers$selector == TRUE,c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, Species, CollectionYear), summarize, mean = round(sum(NumberAdults, na.rm = TRUE)/2 ,4), .drop = TRUE )
+
+cwm_consumers <- ddply(consumers_annual, .(PlotID,CollectionYear), cwm, trait_table = arthropod_traits, traits = c("Body_Size", "Dispersal_ability", "Stratum_use_numeric"))
+
+pairs(cwm_consumers[3:5], pch = 20)
+
+ 
