1693520182fb75812911f037045cbf28c3feb3b2
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-09-15 13:46:17 +0200

    read in compiled arthropod data, split in predators and herbivores

--- code/data.r
+++ code/data.r
@@ -120,31 +120,39 @@ save(plant_trait_matrix, file = "data/plant_trait_matrix.rData")
 
 # read in arthropod  data
 
-IDs <- c(16871, 16893, 16894, 16895, 16897, 16908, 17006, 17007, 17008, 17009, 17010, 17011, 17012, 17013, 17014, 17015, 17266, 17246, 17247, 17267)
+#IDs <- c(16871, 16893, 16894, 16895, 16897, 16908, 17006, 17007, 17008, 17009, 17010, 17011, 17012, 17013, 17014, 17015, 17266, 17246, 17247, 17267)
 
-lapply(IDs, function(x) colnames(read.csv2(paste0("data/arthropods_core/", x, ".txt"), sep = "", header = TRUE, nrows = 1 )))
+#lapply(IDs, function(x) colnames(read.csv2(paste0("data/arthropods_core/", x, ".txt"), sep = "", header = TRUE, nrows = 1 )))
 
-ddf <- lapply(IDs, function(ID) read.csv2(paste0("data/arthropods_core/", ID, ".txt"), sep = "\t") )
+#ddf <- lapply(IDs, function(ID) read.csv2(paste0("data/arthropods_core/", ID, ".txt"), sep = "\t") )
 # original files: ______,____  downloaded from   BExis on ____ as Bexis dataset ________ ; Maintainer: Martin Goßner
 
-arthropods_core <- ddf[[1]]
-for(i in 2:length(IDs)) {
-  arthropods_core <- rbind(arthropods_core, ddf[[i]])
-}
 
-arthropods_core <- subset(arthropods_core, Species != "<NA>")
+#arthropods_core <- ddf[[1]]
+#for(i in 2:length(IDs)) {
+#  arthropods_core <- rbind(arthropods_core, ddf[[i]])
+#}
 
-plots <- unique(plants_full$EP_PlotId)
-levels(arthropods_core$PlotID) <- plots
-arthropod_species <- levels(arthropods_core$Species)
+#arthropods_core <- subset(arthropods_core, Species != "<NA>")
+
+#plots <- unique(plants_full$EP_PlotId)
+#levels(arthropods_core$PlotID) <- plots
+#arthropod_species <- levels(arthropods_core$SpeciesID)
 
 # select replicatess for analysis: chose replicates of June and August sampling, except for Schorfheide sampling in 2009, where sampling took place in June and August. 
-arthropods_core$selector <- with(arthropods_core, (CollectionMonth %in% c("Jun", "Aug") & !(CollectionYear == 2009 & Exploratory == "SCH") | CollectionMonth %in% c("Jul", "Sep") & CollectionYear == 2009 & Exploratory == "SCH" ))
+#arthropods_core$selector <- with(arthropods_core, (CollectionMonth %in% c("Jun", "Aug") & !(CollectionYear == 2009 & Exploratory == "SCH") | CollectionMonth %in% c("Jul", "Sep") & CollectionYear == 2009 & Exploratory == "SCH" ))
 
 #remove replicates without a second counterpart
-replicates <- ddply(arthropods_core[arthropods_core$selector == TRUE, c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, CollectionYear), summarize, n = length(unique(CollectionMonth)), .drop = TRUE )
-arthropods_core$selector[paste(arthropods_core$PlotID, arthropods_core$CollectionYear) %in% paste(replicates$PlotID, replicates$CollectionYear)[replicates$n != 2]] <- FALSE
+#replicates <- ddply(arthropods_core[arthropods_core$selector == TRUE, c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, CollectionYear), summarize, n = length(unique(CollectionMonth)), .drop = TRUE )
+#arthropods_core$selector[paste(arthropods_core$PlotID, arthropods_core$CollectionYear) %in% paste(replicates$PlotID, replicates$CollectionYear)[replicates$n != 2]] <- FALSE
 
+## WARNING: Check calculation of annual means: species that have not been reported should have value 0 at each plot and not be treated as NA.  
+#arthropods_annual <- ddply(arthropods_core[arthropods_core$selector == TRUE,c("PlotID", "Species","CollectionYear","NumberAdults", "CollectionMonth")], .(PlotID, Species, CollectionYear), summarize, mean = round(sum(NumberAdults, na.rm = TRUE)/2 ,4), .drop = TRUE )
+
+
+arthropods_core <- read.csv2("data/arthropods_core/Arthropods_GL_0813.csv")
+predators_core <- subset(arthropods_core, Feeding_guild == "c")
+herbivores_core <- subset(arthropods_core, Feeding_guild %in% c("h","o"))
 
 
 arthropod_traits <- read.csv("data/arthropod_traits/ArthropodSpeciesTraits.csv", sep = ";")
