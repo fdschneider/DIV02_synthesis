73f1540ef9ecc97501e682ebf56eb833d6aa7353
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-04-04 13:11:26 +0200

    filter trees and tall species from abundance data

--- code/import_plant_abundance.r
+++ code/import_plant_abundance.r
@@ -4,6 +4,10 @@
 
 plants_full <- read.csv2("data/plants_core/plantcover.csv") 
 
+try_species <- read.table("data/plants_traits_TRY/TryAccSpecies.txt", sep = "\t", header = TRUE)
+
+load("data/plant_trait_matrix.rData")
+
 # original file: 19686.xlsx  downloaded from BExis on 17.05.2016 as Bexis
 # dataset 19686 vegetation relevés EP 2008-2015 1.2.5 ; Maintainer: Markus
 # Fischer
@@ -13,19 +17,7 @@ levels(plants_full$Year) <- c(2008:2015, 2011)  # fix levels in bexis table
 
 plants_full <- subset(plants_full, Year %in% 2008:2013)  # select years
 
-# remove tree species from list (for extreme trait values assigned to
-# seedlings/saplings):
-
-trees <- c("Acer_sp", "Baumkeimling_sp", "Betula_pendula", "Fraxinus_excelsior", 
-           "Populus_tremula", "Prunus_avium", "Prunus_sp","Prunus_spinosa", 
-           "Quercus_robur", "Tilia_sp")
-
-plants_full <- subset(plants_full, !Species %in% trees, drop = TRUE)
-plants_full$Species <- factor(plants_full$Species)  # define species as factorial
-
 # make species names compatible with TRY species names
-
-try_species <- read.table("data/plants_traits_TRY/TryAccSpecies.txt", sep = "\t", header = TRUE)
 
 plants_full$AccSpeciesNames <- plants_full$Species
 levels(plants_full$AccSpeciesNames) <- sub("_aggr.","",levels(plants_full$AccSpeciesNames))
@@ -37,11 +29,27 @@ levels(plants_full$AccSpeciesNames)  <- sub("_"," ",levels(plants_full$AccSpecie
 ## fuzzy matching
 #try_ids <- sapply(plant_species, function(x) agrep(x, try_species$AccSpeciesName))
 
+
 ## exact matching
-plants_full <- mutate(plants_full, SpeciesID = match(AccSpeciesNames, try_species$AccSpeciesName) )
+plants_full <- plyr::mutate(plants_full, SpeciesID = match(AccSpeciesNames, try_species$AccSpeciesName) )
+
+
+# remove tree species from list (for extreme trait values assigned to
+# seedlings/saplings):
+
+tall <- as.character(try_species$AccSpeciesName[try_species$AccSpeciesID %in% subset(plant_trait_matrix, height >= 2.0)$AccSpeciesID])
+
+
+trees <- c("Acer sp", "Baumkeimling sp", "Betula pendula", "Fraxinus excelsior", 
+           "Populus tremula", "Prunus avium", "Prunus sp","Prunus spinosa", 
+           "Quercus robur", "Tilia sp")
+
+
+plants_full <- subset(plants_full, !AccSpeciesNames %in% unique(c(tall,trees)), drop = TRUE)
+plants_full$Species <- factor(plants_full$Species)  # define species as factorial
 
 save(plants_full, file = "data/plants_full.rData")
 
-rm(try_species, trees, trees)
+rm(try_species, tall, trees, plant_trait_matrix)
 
 #plants_pooledyears <- ddply(plants_full, .(EP_PlotId), summarise, Year = unique(Year), Species = unique(Species), cover = mean(cover), AccSpecies  )
